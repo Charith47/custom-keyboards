e70648982b9fe31d423a9474b41fbfb954a32adc
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-03-23 10:06:43 +0530

    vanilla-ts client basic setup

---
+++ client-vanilla/src/ts/Editor.ts
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+const _DEFAULT_CAMERA = new THREE.PerspectiveCamera(70, 1, 0.01, 1000);
+_DEFAULT_CAMERA.name = 'Camrea';
+_DEFAULT_CAMERA.position.set(50, 50, 50);
+_DEFAULT_CAMERA.lookAt(new THREE.Vector3());
+
+class Editor {
+	static instance: Editor;
+	camera: THREE.PerspectiveCamera;
+	scene: THREE.Scene;
+
+	private constructor() {
+		this.camera = _DEFAULT_CAMERA.clone();
+		this.scene = new THREE.Scene();
+		this.scene.name = 'Scene';
+		this.scene.background = new THREE.Color(0x3C3C3C);
+		
+	}
+
+	static getInstance(): Editor {
+		if (!Editor.instance) {
+			Editor.instance = new Editor();
+		}
+
+		return Editor.instance;
+	}
+
+}
+
+export { Editor };
---
+++ client-vanilla/src/ts/Viewport.ts
@@ -0,0 +1,51 @@
+import * as THREE from 'three';
+import { Editor } from './Editor';
+
+class Viewport {
+	static instance: Viewport;
+
+	editor: Editor; // camera, scene
+	canvas: Element;
+	renderer: THREE.WebGLRenderer;
+
+	private constructor(editor: Editor) {
+		this.editor = editor;
+
+		this.canvas = document.createElement('div');
+		this.canvas.setAttribute('id', 'canvas');
+
+
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setPixelRatio(1.0);
+		this.renderer.setSize(1920, 1080);
+
+		let pointLight = new THREE.PointLight(0xffffff);
+		pointLight.position.set(75, 75, 75);
+
+		let geometry = new THREE.BoxGeometry(10, 20, 30);
+		let material = new THREE.MeshPhongMaterial({ color: 0x0000aa });
+		let cube = new THREE.Mesh(geometry, material);
+
+		this.editor.scene.add(pointLight);
+		this.editor.scene.add(cube);
+
+		this.canvas.appendChild(this.renderer.domElement);
+        console.log('child appended');
+        this.animate();
+	}
+
+	static getInstance(editor: Editor): Viewport {
+		if (!Viewport.instance) {
+			Viewport.instance = new Viewport(editor);
+		}
+		return Viewport.instance;
+	}
+
+	public animate() {
+        //console.log(this.animate)
+		requestAnimationFrame(()=>{this.animate});
+		this.renderer.render(this.editor.scene, this.editor.camera);
+	}
+}
+
+export { Viewport };
